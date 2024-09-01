61a77d7660cf109032a988c77c13112eaed621f7
Ajinkya Muley <avm121104@gmail.com>
2024-09-01 21:48:26 +0530

    Correcting Error

--- server/index.js
+++ server/index.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname1,'/frontend/dist')))
 
     app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname1,'frontend','dist','index.html'))
+        res.sendFile(path.resolve(__dirname1,'client','dist','index.html'))
     })
 }
 else{
